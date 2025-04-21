78c10122b30d9e7ae89ba66bedbf196f48d617fd
Darkselect <doniyor.kurbanov.24@gmail.com>
2025-04-21 10:03:09 +0500

    changed version up to 21

--- src/main/java/uz/consortgroup/userservice/entity/Password.java
+++ src/main/java/uz/consortgroup/userservice/entity/Password.java
@@ -41,7 +41,7 @@ public class Password {
     private String passwordHash;
 
     @Column(name = "is_active", nullable = false)
-    private Boolean isActive = true;
+    private Boolean isActive;
 
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
