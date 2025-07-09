28d9004fd6906fc4ae4a4496fbb5b520fee9b2bf
Darkselect <doniyor.kurbanov.24@gmail.com>
2025-07-10 00:10:14 +0500

    added all mintrud tests

--- src/test/java/uz/consortgroup/userservice/service/saga/BulkPdfUploadSagaTest.java
+++ src/test/java/uz/consortgroup/userservice/service/saga/BulkPdfUploadSagaTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
